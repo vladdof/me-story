5adca8e5252f6f53db1c1fb32632b82bc50ed1d7
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-07-26 22:49:24 +0200

    feat: customize App / main files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root-page">
+    <the-header />
+    <main class="content">
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+  @import './assets/style.css';
+
+  .root-page {
+    inline-size: 100%;
+    margin-inline: auto;
+    padding-inline: 1.5rem;
+  }
+
+  .content {
+    margin-block-start: 1.5rem;
+    padding-block: 1.5rem;
+  }
+</style>
