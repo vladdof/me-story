c62f7120ad20daba3499dbe4c89872a8925a2900
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-07-26 22:48:04 +0200

    feat: add ROUTES const

---
+++ src/constants/routes.js
@@ -0,0 +1,22 @@
+export const ROUTES = {
+  ROOT: {
+    PATH: '/',
+    NAME: 'home',
+  },
+  HIDDEN: {
+    PATH: '/hidden',
+    NAME: 'hidden',
+  },
+  PUBLIC: {
+    PATH: '/blog',
+    NAME: 'blog',
+  },
+  SIGN_UP: {
+    PATH: '/sign-up',
+    NAME: 'sign-up',
+  },
+  LOGIN: {
+    PATH: '/login',
+    NAME: 'login',
+  },
+};
