4fee9f604256b4a4ee74e08cb46f7506d8d4b558
Vladislav Efanov <vladislav.efanov@gmail.com>
2023-09-01 16:01:15 +0200

    feat: create cmps: ErrorMessage, TheButton and TheSpinner

---
+++ src/components/error-message.vue
@@ -0,0 +1,22 @@
+<template>
+  <p
+    v-if="$slots.default"
+    class="error-message"
+  >
+    <slot />
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'error-message',
+};
+</script>
+
+<style scoped>
+  .error-message {
+    margin-bottom: 16px;
+    font-size: .85rem;
+    color: #990808
+  }
+</style>
---
+++ src/components/the-button.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    class="the-button"
+    :type="type"
+  >
+    <the-spinner v-if="isLoading" />
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+import TheSpinner from './the-spinner.vue';
+
+export default {
+  name: 'the-button',
+  components: { TheSpinner },
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'submit',
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .the-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    min-height: 40px;
+    padding: 8px 10px;
+    font-size: 1rem;
+    color: #fff;
+    border-radius: 10px;
+    border: none;
+    background: #f85f44;
+    outline: none;
+    cursor: pointer;
+    transition: background .2s;
+  }
+
+  .the-button:hover {
+    background: #ea472b;
+  }
+</style>
